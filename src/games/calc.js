8f4ea272efe37db8d2a047aa53e04e55e48cf25d
Daniil-Seleznev <seleznve.d.ole@gmail.com>
2026-03-25 21:12:15 +0300

    added solution

---
+++ src/games/calc.js
@@ -0,0 +1,41 @@
+import runGame from '../index.js';
+
+const description = 'What is the result of the expression?';
+
+const getRandomNumber = () => Math.floor(Math.random() * 100);
+
+const getRandomOperator = () => {
+  const operators = ['+', '-', '*'];
+  const index = Math.floor(Math.random() * operators.length);
+  return operators[index];
+};
+
+const calculate = (a, b, operator) => {
+  switch (operator) {
+    case '+':
+      return String(a + b);
+    case '-':
+      return String(a - b);
+    case '*':
+      return String(a * b);
+    default:
+      throw new Error('Unknown operator');
+  }
+};
+
+const getRoundData = () => {
+  const num1 = getRandomNumber();
+  const num2 = getRandomNumber();
+  const operator = getRandomOperator();
+
+  const question = `${num1} ${operator} ${num2}`;
+  const correctAnswer = calculate(num1, num2, operator);
+
+  return [question, correctAnswer];
+};
+
+const runCalcGame = () => {
+  runGame(description, getRoundData);
+};
+
+export default runCalcGame;
---
+++ src/games/prime.js
@@ -0,0 +1,32 @@
+import runGame from '../index.js';
+
+// Описание игры
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+// Генерация случайного числа
+const getRandomNumber = (min = 2, max = 100) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+// Предикат: проверка, простое ли число
+const isPrime = (num) => {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+// Генерация данных для одного раунда
+const getRoundData = () => {
+  const number = getRandomNumber();
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+  const question = String(number);
+  return [question, correctAnswer];
+};
+
+// Функция запуска игры
+const runPrimeGame = () => {
+  runGame(description, getRoundData);
+};
+
+// Экспортируем как default
+export default runPrimeGame;
